793d9209233c0711bf27caaafc7ecc429bf837da
heizo <onihei.kato@gmail.com>
2018-10-21 18:31:19 +0900

    add sleep after reboot

--- script/update.sh
+++ script/update.sh
@@ -49,4 +49,5 @@ echo "UseDNS no" >> /etc/ssh/sshd_config
 echo "====> Shutting down the SSHD service and rebooting..."
 systemctl stop sshd.service
 nohup shutdown -r now < /dev/null > /dev/null 2>&1 &
+sleep 120
 exit 0
