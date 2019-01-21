3a9550adf08d946e28375203c442486ac5f0ffdc
heizo <onihei.kato@gmail.com>
2019-01-22 06:48:48 +0900

    Remove ubuntu-release-upgrader-core package

--- script/update.sh
+++ script/update.sh
@@ -11,7 +11,7 @@ update-locale LANG=$LANG LC_ALL=$LC_ALL
 
 # Disable the release upgrader
 echo "==> Disabling the release upgrader"
-sed -i.bak 's/^Prompt=.*$/Prompt=never/' /etc/update-manager/release-upgrades
+apt-get -y purge ubuntu-release-upgrader-core
 
 echo "==> Disabling apt.daily.service"
 systemctl stop apt-daily.timer
