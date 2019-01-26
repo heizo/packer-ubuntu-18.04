31ad004bcb03a9c47d516fe5986ecf582344a25b
heizo <onihei.kato@gmail.com>
2019-01-26 09:00:48 +0900

    Reduce code

--- script/minimize.sh
+++ script/minimize.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -eu
 
-echo "==> Disk usage before minimization"
-df -h
-
-echo "==> Installed packages before cleanup"
-dpkg --get-selections | grep -v deinstall
-
 # Remove some packages to get a minimal install
 echo "==> Removing all linux kernels except the currrent one"
 apt-get -y purge `ls /boot/vmlinuz-* | sed -e '$d' | sed s/.*vmlinuz/linux-image/`
@@ -46,6 +40,3 @@ echo "==> Removing any docs"
 rm -rf /usr/share/doc/*
 echo "==> Removing caches"
 find /var/cache -type f -exec rm -rf {} \;
-
-echo "==> Disk usage after cleanup"
-df -h
