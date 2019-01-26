3342e7ede5604899c6c899b322a9b7971f26d81d
heizo <onihei.kato@gmail.com>
2019-01-26 13:12:34 +0900

    Clean unused kernel

--- script/minimize.sh
+++ script/minimize.sh
@@ -11,9 +11,7 @@ update-locale LANG=$LANG LC_ALL=$LC_ALL
 
 # Remove some packages to get a minimal install
 echo "==> Removing all linux kernels except the currrent one"
-apt-get -y purge `ls /boot/vmlinuz-* | sed -e '$d' | sed s/.*vmlinuz/linux-image/`
-apt-get -y purge $(dpkg --list |egrep 'linux-image-[0-9]' |awk '{print $3,$2}' |sort -nr |tail -n +2 |grep -v $(uname -r) |awk '{ print $2}')
-apt-get -y purge $(dpkg --list |grep '^rc' |awk '{print $2}')
+dpkg --list 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs apt-get -y purge
 echo "==> Removing linux source"
 dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
 echo "==> Removing documentation"
@@ -25,10 +23,11 @@ dpkg --list | awk '{ print $2 }' | grep -- '-doc$' | xargs apt-get -y purge
 #apt-get -y purge cpp gcc g++
 #apt-get -y purge build-essential git
 echo "==> Removing X11 libraries"
-apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
+apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6 libxau6 libxdmcp6
 echo "==> Removing other oddities"
-apt-get -y purge popularity-contest installation-report landscape-common wireless-tools wpasupplicant ubuntu-serverguide
+apt-get -y purge popularity-contest installation-report plymouth xdg-user-dirs
 apt-get -y purge nano
+apt-get -y purge $(dpkg --list |grep '^rc' |awk '{print $2}')
 
 # Clean up orphaned packages with deborphan
 apt-get -y install deborphan
