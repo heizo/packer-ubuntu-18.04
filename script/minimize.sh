6a3429cd3a12cdf33332fd3d0cc37025b8ea11a2
heizo <onihei.kato@gmail.com>
2019-02-09 13:01:46 +0900

    Remove white-space

--- script/minimize.sh
+++ script/minimize.sh
@@ -13,9 +13,9 @@ update-locale LANG=$LANG LC_ALL=$LC_ALL
 echo "==> Removing all linux kernels except the currrent one"
 dpkg --list 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs apt-get -y purge
 echo "==> Removing linux source"
-dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
+dpkg --list | awk '{print $2}' | grep linux-source | xargs apt-get -y purge
 echo "==> Removing documentation"
-dpkg --list | awk '{ print $2 }' | grep -- '-doc$' | xargs apt-get -y purge
+dpkg --list | awk '{print $2}' | grep -- '-doc$' | xargs apt-get -y purge
 echo "==> Removing X11 libraries"
 apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6 libxau6 libxdmcp6
 echo "==> Removing other oddities"
