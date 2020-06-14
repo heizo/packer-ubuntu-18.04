db3fd6362d4e9f4056217e34effa55484ef83fba
heizo <onihei.kato@gmail.com>
2020-06-14 21:32:48 +0900

    Truncate /etc/machine-id

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -17,6 +17,9 @@ unset HISTFILE
 rm -f /root/.bash_history
 rm -f /home/${SSH_USER}/.bash_history
 
+echo "==> Clearing machine-id"
+truncate --size=0 /etc/machine-id
+
 echo "==> Clearing log files"
 find /var/log -type f -exec truncate --size=0 {} \;
 
